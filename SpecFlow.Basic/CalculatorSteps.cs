5fa355dc8fb91f48511a79920420fb0ee2b8cd0c
Saymon C.A.O <Saymon C.A.O>
2017-03-15 00:43:04 -0300

    Primeira visão SpecFlow

---
+++ SpecFlow.Basic/CalculatorSteps.cs
@@ -0,0 +1,42 @@
+﻿using Example;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow.Basic
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+
+        private Calculator calculator = new Calculator();
+        private int result { get; set; }
+
+
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+            Console.WriteLine(result);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
